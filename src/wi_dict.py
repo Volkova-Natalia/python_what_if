e31c6d79be19a5ff7e6c6570c3e16c58e2f3066c
Volkova-Natalia <volkova.natalia.1988@gmail.com>
2022-02-13 17:11:19 +0300

    (refactor)

--- src/wi_dict.py
+++ src/wi_dict.py
@@ -1,19 +1,26 @@
 """
 ----- Hash collisions: -----
 
->>> hash(1) == hash(1.0) == hash(True) == 1
+>>> (hash(1)    == 1   and
+...  hash(1.0)  == 1   and
+...  hash(True) == 1)
 True
 
->>> my_dict = {1: 'val_1', 1.0: 'val_2', True: 'val_3'};   print(my_dict)
+>>> my_dict = {1: 'val_1', 1.0: 'val_2', True: 'val_3'}
+>>> my_dict
 {1: 'val_3'}
 
->>> my_dict = {};   print(my_dict)
+>>> my_dict = {}
+>>> my_dict
 {}
->>> my_dict[1.0]  = 'val_1';   print(my_dict)
+>>> my_dict[1.0]  = 'val_1'
+>>> my_dict
 {1.0: 'val_1'}
->>> my_dict[1]    = 'val_2';   print(my_dict)
+>>> my_dict[1]    = 'val_2'
+>>> my_dict
 {1.0: 'val_2'}
->>> my_dict[True] = 'val_3';   print(my_dict)
+>>> my_dict[True] = 'val_3'
+>>> my_dict
 {1.0: 'val_3'}
 """
 
--- src/wi_hash.py
+++ src/wi_hash.py
@@ -1,24 +1,24 @@
 """
->>> hash(0)     == 0   and\
-    hash(-0)    == 0   and\
-    hash(0.0)   == 0   and\
-    hash(False) == 0   and\
-    hash("")    == 0
+>>> (hash(0)     == 0   and
+...  hash(-0)    == 0   and
+...  hash(0.0)   == 0   and
+...  hash(False) == 0   and
+...  hash("")    == 0)
 True
 
->>> hash(1)    == 1   and\
-    hash(1.0)  == 1   and\
-    hash(True) == 1
+>>> (hash(1)    == 1   and
+...  hash(1.0)  == 1   and
+...  hash(True) == 1)
 True
 
->>> hash(-1)   == -2   and\
-    hash(-1.0) == -2
+>>> (hash(-1)   == -2   and
+...  hash(-1.0) == -2)
 True
 
->>> hash(None) != 0   and\
-    hash("0")  != 0   and\
-    hash(" ")  != 0   and\
-    hash("1")  != 1
+>>> (hash(None) != 0   and
+...  hash("0")  != 0   and
+...  hash(" ")  != 0   and
+...  hash("1")  != 1)
 True
 
 >>> hash(tuple())
@@ -26,24 +26,24 @@ True
 >>> hash((tuple(), ))
 -5486347211504344842
 
->>> hash((0, ))     == \
-    hash((-0, ))    == \
-    hash((0.0, ))   == \
-    hash((False, )) == \
-    hash(("", ))    == \
-    -8753497827991233192
+>>> (hash((0, ))     ==
+...  hash((-0, ))    ==
+...  hash((0.0, ))   ==
+...  hash((False, )) ==
+...  hash(("", ))    ==
+...  -8753497827991233192)
 True
 
->>> hash((1, ))    == \
-    hash((1.0, ))  == \
-    hash((True, )) == \
-    -6644214454873602895
+>>> (hash((1, ))    ==
+...  hash((1.0, ))  ==
+...  hash((True, )) ==
+...  -6644214454873602895)
 True
 
->>> hash((None, )) != hash((0, ))    and\
-    hash(("0", ))  != hash((0, ))    and\
-    hash((" ", ))  != hash(("", ))   and\
-    hash(("1", ))  != hash((1, ))
+>>> (hash((None, )) != hash((0, ))    and
+...  hash(("0", ))  != hash((0, ))    and
+...  hash((" ", ))  != hash(("", ))   and
+...  hash(("1", ))  != hash((1, )))
 True
 """
 
